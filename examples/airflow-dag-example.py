fca925ad254723382599de49acf49d32437bd782
Guilherme-Silveira <guilhermesilveira.s@hotmail.com>
2022-12-10 18:13:56 -0300

    update example for docker

--- examples/airflow-dag-example.py
+++ examples/airflow-dag-example.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta
-from os import getenv
 from airflow.models import DAG
-from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.operators.docker_operator import DockerOperator
 
 
 default_args = {
@@ -15,29 +13,23 @@ default_args = {
   'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG(
-  'test-spark-pipeline',
-  default_args=default_args,
-  start_date=datetime.now(),
-  schedule_interval='@weekly',
-  tags=['test', 'development', 'bash']
-)
+with DAG(
+    'test-spark-pipeline',
+    default_args=default_args,
+    start_date=datetime.now(),
+    schedule_interval='@weekly',
+    tags=['test', 'development', 'bash']
+  ) as dag:
 
-test_application_spark = SparkKubernetesOperator(
-  task_id ='test_application_spark',
-  namespace ='bigdata',
-  application_file='manifests/test-application.yaml',
-  kubernetes_conn_id='k3d',
-  do_xcom_push=True,
-  dag=dag
-)
+  t1 = DockerOperator(
+        task_id='spark-airflow-test',
+        image='guisilveira/test-application-spark-docker',
+        container_name='spark-airflow-test',
+        api_version='auto',
+        auto_remove=True,
+        command="/opt/spark/bin/spark-submit /app/test-application.py",
+        docker_url="tcp://docker-proxy:2375",
+        network_mode="bigdata-docker_bigdata"
+        )
 
-monitor_spark_app_status = SparkKubernetesSensor(
-  task_id='monitor_spark_app_status',
-  namespace='bigdata',
-  application_name="{{ task_instance.xcom_pull(task_ids='test_application_spark')['metadata']['name'] }}",
-  kubernetes_conn_id='k3d',
-  dag=dag
-)
-
-test_application_spark >> monitor_spark_app_status
+  t1
--- examples/test-application/build/build-image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t guisilveira/test-application-spark-docker .
-docker push guisilveira/test-application-spark-docker
